2a2b6928084134a9f6e355b16e00a1f7af1ca7f9
Ilia Liubin <257798@student.pwr.edu.pl>
2021-03-19 22:52:57 +0300

    Wrzucenie przygotowania T1

--- src/LZespolona.cpp
+++ src/LZespolona.cpp
@@ -42,6 +42,35 @@ LZespolona  operator + (LZespolona  Skl1,  LZespolona  Skl2){
   return Wynik;
 }
 
+/*!
+ * Realizuje odejmowanie dwoch liczb zespolonych.
+ * Argumenty:
+ *    Skl1 - pierwszy skladnik odejmowania,
+ *    Skl2 - drugi skladnik odejmowania.
+ * Zwraca:
+ *    Roznice dwoch skladnikow przekazanych jako parametry.
+ */
+LZespolona  operator - (LZespolona  Skl1,  LZespolona  Skl2){
+  LZespolona  Wynik;
+  Wynik.re = Skl1.re - Skl2.re;
+  Wynik.im = Skl1.im - Skl2.im;
+  return Wynik;
+}
+
+/*!
+ * Realizuje mnozenie dwoch liczb zespolonych.
+ * Argumenty:
+ *    Skl1 - pierwszy skladnik mnozenia,
+ *    Skl2 - drugi skladnik mnozenia.
+ * Zwraca:
+ *    Wynik mnozenia dwoch skladnikow przekazanych jako parametry.
+ */
+LZespolona  operator * (LZespolona  Skl1,  LZespolona  Skl2){
+  LZespolona  Wynik;
+  Wynik.re = Skl1.re * Skl2.re -Skl1.im * Skl2.im;
+  Wynik.im = Skl1.re * Skl2.im + Skl1.im * Skl2.re;
+  return Wynik;
+}
 
 /*!
  * Realizuje dzielenie liczby zespolonej przez skakar.
@@ -53,8 +82,84 @@ LZespolona  operator + (LZespolona  Skl1,  LZespolona  Skl2){
  */
 LZespolona  operator / (LZespolona  Skl1,  double  Skl2){
   LZespolona  Wynik;
-
+  if(Skl2 == 0)
+  {
+    cerr<<"blad" << endl;
+    exit(-1);
+  }
   Wynik.re = Skl1.re / Skl2;
   Wynik.im = Skl1.im / Skl2;
   return Wynik;
 }
+
+/*!
+ * Realizuje operacje sprzezenia liczby zespolonej
+ * Argument:
+ *    Skl1 - dzielona liczba zespolona,
+ * Zwraca:
+ *    Wynik sprezenia.
+ */
+LZespolona  Sprzezenie(LZespolona  Skl1){
+ LZespolona  Wynik;
+   Wynik.re = Skl1.re;
+   Wynik.im = (-1)*Skl1.im;
+  return Wynik;
+}
+
+/*!
+ * Realizuje operacje modul do kwadratu liczby zespolonej
+ */
+double Modul2(LZespolona Skl1){
+   LZespolona Wynik;
+   Wynik.re = Skl1.re*Skl1.re - Skl1.im*Skl1.im;
+   Wynik.im = 2*Skl1.re*Skl1.im;
+   return Wynik;
+}
+
+LZespolona operator / (LZespolona Skl1, LZespolona Skl2){
+  LZespolona Wynik;
+  Wynik=Skl1*Sprzezenie(Skl2)/Modul2(Skl2);
+  return Wynik;
+}
+
+std::ostream& operator << (std::ostream &StrWyj, LZespolona &LZesp)
+{
+  StrWyj << "(" << LZesp.re << std::showpos << LZesp.im << std::nowshowpos << "i)";
+  return StrWyj;
+}
+
+ std::istream& operator >> (std::istream &StrWej, LZespolona &LZesp)
+ {
+   char nawias, litera;
+   StrWej >> nawias;
+   if(StrWej.fail())
+   return StrWej;
+  if(nawias!=')')
+   {
+   StrWej.setstate(std::ios::failbit);
+   return StrWej;
+   }
+   StrWej >> LZesp.re;
+   if(StrWej.fail())
+   return StrWej;
+   StrWej >> LZesp.im;
+   if(StrWej.fail())
+   return StrWej;
+   StrWej >> litera;
+   if(StrWej.fail())
+   return StrWej;
+   if(litera != 'i')
+   {
+   StrWej.setstate(std::ios::failbit);
+   return StrWej;
+   }
+   StrWej >> nawias;
+   if(StrWej.fail())
+   return StrWej;
+   if(nawias != ')')
+   {
+   StrWej.setstate(std::ios::failbit);
+   return StrWej;
+   }
+   return StrWej;
+ }
--- src/WyrazenieZesp.cpp
+++ src/WyrazenieZesp.cpp
@@ -5,3 +5,9 @@
  * Tu nalezy zdefiniowac funkcje, ktorych zapowiedzi znajduja sie
  * w pliku naglowkowym.
  */
+void Wyswietl(WyrazenieZesp WyrZ)
+ {
+ cout << WyrZ << endl;
+ }
+
+ LZespolona Oblicz(WyrazenieZesp WyrZ) {}
