4252c103904c1c0f00091f0e128101f48d1ef362
Ilia Liubin <257798@student.pwr.edu.pl>
2021-04-01 00:32:59 +0300

    Wrzucenie testow

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,7 +1,7 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "./doctest/doctest.h"
 #include "LZespolona.hh"
-
+#include "WyrazenieZesp.hh"
 TEST_CASE("test LZespolona Suma"){
     LZespolona x, y, z;
 
@@ -13,6 +13,236 @@ TEST_CASE("test LZespolona Suma"){
 
     z.re = 2;
     z.im = -2;
-    
+
     CHECK(x+y == z);
 }
+
+TEST_CASE("test LZespolona Suma"){
+    LZespolona x, y, z;
+
+    x.re = 1;
+    x.im = 1;
+
+    y.re = 0;
+    y.im = 0;
+
+    z.re = 1;
+    z.im = 1;
+
+    CHECK(x+y == z);
+}
+
+TEST_CASE("test LZespolona Suma"){
+    LZespolona x, y, z;
+
+    x.re = 1;
+    x.im = 1;
+
+    y.re = 0.00001;
+    y.im = 0.00001;
+
+    z.re = 1;
+    z.im = 1;
+
+    CHECK(x+y == z);
+}
+
+TEST_CASE("test LZespolona Suma"){
+    LZespolona x, y, z;
+
+    x.re =0.00009;
+    x.im = 0.00009;
+
+    y.re = 0.00001;
+    y.im = 0.00001;
+
+    z.re = 0;
+    z.im = 0;
+
+    CHECK(x+y == z);
+}
+
+TEST_CASE("test LZespolona Roznica"){
+    LZespolona x, y, z;
+
+    x.re = 3;
+    x.im = 1;
+
+    y.re = 2;
+    y.im = -3;
+
+    z.re = 1;
+    z.im = 4;
+
+    CHECK(x-y == z);
+}
+TEST_CASE("test LZespolona Roznica"){
+    LZespolona x, y, z;
+
+    x.re = 1;
+    x.im = 1;
+
+    y.re = 0.00001;
+    y.im = 0.00001;
+
+    z.re = 1;
+    z.im = 1;
+
+    CHECK(x-y == z);
+}
+
+TEST_CASE("test LZespolona Roznica"){
+    LZespolona x, y, z;
+
+    x.re =0.49999;
+    x.im = 0.49999;
+
+    y.re = 0.00001;
+    y.im = 0.00001;
+
+    z.re = 0.5;
+    z.im = 0.5;
+
+    CHECK(x-y == z);
+}
+TEST_CASE("test LZespolona Iloczyn"){
+    LZespolona x, y, z;
+
+    x.re = 3;
+    x.im = -3;
+
+    y.re = 1;
+    y.im = 2;
+
+    z.re = 9;
+    z.im = 3;
+
+    CHECK(x*y == z);
+}
+TEST_CASE("test LZespolona Iloczyn"){
+    LZespolona x, y, z;
+
+    x.re = 1;
+    x.im = 1;
+
+    y.re = 0;
+    y.im = 0;
+
+    z.re = 0;
+    z.im = 0;
+
+    CHECK(x*y == z);
+}
+
+TEST_CASE("test LZespolona Iloczyn"){
+    LZespolona x, y, z;
+
+    x.re = 1;
+    x.im = 1;
+
+    y.re = 1;
+    y.im = 0;
+
+    z.re = 1;
+    z.im = 1;
+
+    CHECK(x*y == z);
+}
+
+TEST_CASE("test LZespolona Iloczyn"){
+    LZespolona x, y, z;
+
+    x.re = 1;
+    x.im = 1;
+
+    y.re = 0;
+    y.im = 1;
+
+    z.re = -1;
+    z.im = 1;
+
+
+    CHECK(x*y == z);
+}
+
+
+TEST_CASE("test LZespolona Iloczyn"){
+    LZespolona x, y, z;
+
+    x.re = 1;
+    x.im = 1;
+
+    y.re = 1;
+    y.im = 1;
+
+    z.re = 0;
+    z.im = 2;
+
+
+    CHECK(x*y == z);
+}
+
+TEST_CASE("test LZespolona Iloczyn"){
+    LZespolona x, y, z;
+
+    x.re = 1;
+    x.im = 1;
+
+    y.re = 0.00001;
+    y.im = 0.00001;
+
+    z.re = 0;
+    z.im = 0;
+
+
+    CHECK(x*y == z);
+}
+
+TEST_CASE("test LZespolona Iloraz"){
+    LZespolona x, y, z;
+
+    x.re = 3;
+    x.im = -3;
+
+    y.re = 1;
+    y.im = 2;
+
+    z.re = 9;
+    z.im = 3;
+
+    CHECK(x/y == z);
+}
+
+TEST_CASE("Test LZespolona dodawanie,znany wynik") {
+    WyrazenieZesp x{{1,1},Op_Dodaj,{1,1}};
+    LZespolona t ={2,2};
+
+
+    CHECK(Oblicz(x)== t);
+}
+
+TEST_CASE("Test LZespolona odejmowanie, znany wynik") {
+    WyrazenieZesp x{{1,1},Op_Odejmij,{1,1}};
+    LZespolona t ={0,0};
+
+
+    CHECK(Oblicz(x)== t);
+}
+
+TEST_CASE("Test LZespolona mnozenie, znany wynik") {
+    WyrazenieZesp x{{1,1},Op_Mnoz,{1,1}};
+    LZespolona t ={0,2};
+
+
+    CHECK(Oblicz(x)== t);
+}
+
+TEST_CASE("Test LZespolona dzielenie, znany wynik") {
+    WyrazenieZesp x{{1,1},Op_Dziel,{1,1}};
+    LZespolona t ={1,0};
+
+
+    CHECK(Oblicz(x)== t);
+}
+
+
--- tests/test2.cpp
+++ tests/test2.cpp
@@ -5,36 +5,95 @@
 TEST_CASE("Test LZespolona dzielenie przez skalar 1") {
     LZespolona x, y;
     double t = 2;
-    
+
     x.re = 2;
     x.im = 2;
 
     y.re = 1;
     y.im = 1;
-   
+
     CHECK(x/t == y);
 }
 
 TEST_CASE("Test LZespolona dzielenie przez skalar 2") {
     LZespolona x, y;
     double t = 3;
-    
+
     x.re = 2;
     x.im = 2;
 
     y.re = 1;
     y.im = 1;
-   
+
     CHECK(x/t == y);
 }
 
 TEST_CASE("Test LZespolona dzielenie przez skalar - zero") {
     LZespolona x;
     double t = 0;
-    
+
     x.re = 2;
     x.im = 2;
- 
+
    WARN_THROWS(x/t);
 }
 
+TEST_CASE("LZespolona - wyswietlanie standard") {
+    LZespolona x;
+
+    x.re = 2;
+    x.im = 2;
+
+    std::ostringstream out;
+
+    out << x;
+    std::cout << out.str() << std::endl;
+    CHECK("(2.00+2.00i)" == out.str() );
+}
+
+TEST_CASE("LZespolona - wyswietlanie zaokraglane") {
+    LZespolona x;
+
+    x.re = 2.0/3.0;
+    x.im = 2.0/3.0;
+
+    std::ostringstream out;
+
+    out << x;
+    std::cout << out.str() << std::endl;
+    CHECK("(0.67+0.67i)" == out.str() );
+}
+
+TEST_CASE("LZespolona - wczytywanie standard") {
+    LZespolona x;
+
+    std::istringstream in("(10+10.10i)");
+    in >> x;
+
+    std::ostringstream out;
+    out << x;
+
+    CHECK("(10.00+10.10i)" == out.str() );
+}
+
+TEST_CASE("test LZespolona modul"){
+    LZespolona x;
+    double s=2;
+    x.re = 1;
+    x.im = 1;
+
+
+    CHECK( Modul2(x) == s);
+}
+
+TEST_CASE("test LZespolona sprzezenie"){
+    LZespolona x, y;
+
+    x.re = 1;
+    x.im = 1;
+
+    y.re = 1;
+    y.im = -1;
+
+    CHECK(Sprzezenie(x) == y);
+}
